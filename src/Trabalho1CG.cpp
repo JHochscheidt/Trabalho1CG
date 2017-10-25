ce27f582d16d02e00617abd59b88886df31b02ef
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2017-10-25 21:05:26 -0200

    posicao da camera padrao e da camera atras do personagem funcionando. VERIFICAR ZOOM IN e ZOOM OUT na camera do ROBO

--- src/Trabalho1CG.cpp
+++ src/Trabalho1CG.cpp
@@ -17,10 +17,12 @@
 #define Z 2
 #define CONE 2
 #define TORUS 3
-#define ALTURA_PAREDE 2
+#define ALTURA_PAREDE 3
 #define TEAPOT 4
-#define DIREITA 1
-#define ESQUERDA 2
+#define CAM_PADRAO 2
+#define CAM_TRAS 3
+#define CAM_ROBO 4
+
 #define CIMA 3
 #define BAIXO 4
 
@@ -107,18 +109,16 @@ double angleRotateRobot = 0;
 double rotateRobo = 0;
 double rotateArms = 0;
 double rotateHead = 0;
-int CAM_ATUAL = 0;
+int CAM_ATUAL = CAM_PADRAO;
 bool ROT_ARM_FRONT = true;
 bool ROT_HEAD_RIGHT = true;
 int posRobo[3] = {22,0,0};
 
-#define ROBO 9
 double camera[3] = {hMaze/2,wMaze/2,100};
 double focus[3] = {hMaze/2,wMaze/2,0}; // para onde a camera esta olhando
 int rotMaze[3] = {0,0,0};
 double rotateMaze = 0;
 int trMaze[3] = {0,0,0};
-
 GLUquadricObj *novaQuadrica();
 void reshapeWindow(GLsizei w, GLsizei h);
 void draw();
@@ -156,24 +156,20 @@ void draw(){
 	glLoadIdentity();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	if(CAM_ATUAL == ROBO){
-		camera[X] = posRobo[X];
-		camera[Y] = posRobo[Y] - 10;
-		camera[Z] = 15;
-		gluLookAt(camera[X],camera[Y], camera[Z], posRobo[X],posRobo[Y], posRobo[Z], 0,1,0);
-	}else{
-		gluLookAt(camera[X],camera[Y], camera[Z], focus[X],focus[Y],focus[Z], 0,1,0);
+	switch(CAM_ATUAL){
+		case CAM_PADRAO: //camera padrao olhando o labirinto de cima
+			gluLookAt(camera[X],camera[Y],camera[Z], focus[X], focus[Y], focus[Z], 0,1,0);
+			break;
+		case CAM_ROBO: //camera que foca o robo
+			gluLookAt(posRobo[X], posRobo[Y]-10, posRobo[Z]+20, posRobo[X], posRobo[Y], posRobo[Z], 0,1,0);
+			break;
 	}
 
 	glColor3f(0.5,0.75,0.25);
-
 	//rotacoes com o teclado
 	glRotatef(-rotX, 1,0,0);
 	glRotatef(-rotY, 0,1,0);
 	glRotatef(-rotZ, 0,0,1);
-
-
-
 	//CHAO PLANO XY
 	glBegin(GL_QUADS);
 		glVertex3f(0,0,0);
@@ -181,84 +177,37 @@ void draw(){
 		glVertex3f(hMaze,wMaze,0);
 		glVertex3f(0,wMaze,0);
 	glEnd();
-	glBegin(GL_QUADS); //PLANO XZ
-		glColor3f(1,1,0.5);
-		glVertex3f(0,0,0);
-		glVertex3f(0,0,10);
-		glVertex3f(10,0,10);
-		glVertex3f(10,0,0);
-	glEnd();
-	glBegin(GL_QUADS); //PLANO YZ
-		glColor3f(0.5,0.5,0.5);
-		glVertex3f(0,0,0);
-		glVertex3f(0,10,0);
-		glVertex3f(0,10,10);
-		glVertex3f(0,0,10);
-
-	glEnd();
-
-
-	//LABIRINTO
+//	glBegin(GL_QUADS); //PLANO XZ glColor3f(1,1,0.5); glVertex3f(0,0,0); glVertex3f(0,0,10); glVertex3f(10,0,10); glVertex3f(10,0,0);
+//	glEnd();
+//	glBegin(GL_QUADS); //PLANO YZ glColor3f(0.5,0.5,0.5); glVertex3f(0,0,0); glVertex3f(0,10,0); glVertex3f(0,10,10); glVertex3f(0,0,10);
+//	glEnd();
 	glColor3f(1,0,0);
 	drawMaze(wMaze, hMaze);
-
-
-	//ROBO
 	drawRobot();
-
 	glutSwapBuffers();
-
 	glutPostRedisplay();
-
 	//MOVIMENTOS DA CABEÇA
 	if(ROT_HEAD_RIGHT == true){
-		//rotaciona ate chegar no 30
-		if(rotateHead < 30){
-			rotateHead = rotateHead + 1;
-		}else{
-			ROT_HEAD_RIGHT = false;
-		}
+		if(rotateHead < 30){ rotateHead = rotateHead + 1;
+		}else{ ROT_HEAD_RIGHT = false; }
 	}else if(ROT_HEAD_RIGHT == false){
-		if(rotateHead > -30){
-			rotateHead = rotateHead - 1;
-		}else{
-			ROT_HEAD_RIGHT = true;
-		}
+		if(rotateHead > -30){ rotateHead = rotateHead - 1;
+		}else{ ROT_HEAD_RIGHT = true; }
 	}
-
 	//MOVIMENTOS DOS BRAÇOS
 	if(ROT_ARM_FRONT == true){
-		if(rotateArms < 30){
-			rotateArms = rotateArms + 0.5;
-		}else{
-			ROT_ARM_FRONT = false;
-		}
+		if(rotateArms < 15){ rotateArms = rotateArms + 0.5;
+		}else{ ROT_ARM_FRONT = false; }
 	}else if(ROT_ARM_FRONT == false){
-		if(rotateArms > -30){
-			rotateArms = rotateArms - 0.5;
-		}else{
-			ROT_ARM_FRONT = true;
-		}
+		if(rotateArms > -15){ rotateArms = rotateArms - 0.5;
+		}else{ ROT_ARM_FRONT = true; }
 	}
-
 }
 
 void animate(int value){
-
-	//rotaciona o robo
-	if(rotateRobo == 90 && angleRotateRobot < 90){
-		angleRotateRobot+=5;
-	}else if(rotateRobo == 180 && angleRotateRobot < 180){
-		angleRotateRobot+=5;
-	}else if(rotateRobo == -90 && angleRotateRobot > -90){
-		angleRotateRobot-=5;
-	}else{
-		angleRotateRobot = 0;
-	}
-
 	glutPostRedisplay();
 	draw();
-	glutTimerFunc(250,animate,1);
+	glutTimerFunc(60,animate,1);
 }
 
 void teclado(unsigned char key, int x, int y){
@@ -269,26 +218,24 @@ void teclado(unsigned char key, int x, int y){
 		case 'Q':
 			exit(0);
 			break;
-		case '2': // rotaciona -X
-			rotX--;
-			break;
-		case '8': //rotaciona X
-			rotX++;
-			break;
-		case '4': //rotaciona -Y
-			rotY--;
-			break;
-		case '6': //rotaciona Y
-			rotY++;
-			break;
 		case '-': //zoom in
-			camera[Y]+=1;
-			camera[Z]-=2;
+			if(CAM_ATUAL == CAM_PADRAO){
+				printf("Z%f Y%f\n", camera[Z], camera[Y]);
+				if(camera[Y] > -30){
+					camera[Z]-=0.5;
+					camera[Y]--;
+				}
+			}
 			break;
 		case '+': //zoom out
-			camera[Y]-=1;
-			camera[Z]+=2;
-
+			if(CAM_ATUAL == CAM_PADRAO){
+				printf("Z%f Y%f\n", camera[Z], camera[Y]);
+				if(camera[Z] < 100){
+					camera[Z]+=0.5;
+					camera[Y]++;
+				}
+			}
+			break;
 	}
 	glutPostRedisplay();
 }
@@ -309,136 +256,18 @@ void specialKeys(int key, int x, int y){
 			}
 			break;
 		case GLUT_KEY_F2: //camera atras do robo e mostrando todo o labirinto
-			CAM_ATUAL = ROBO; //camera atras do robo
+			CAM_ATUAL = CAM_PADRAO;
+			camera[X] = hMaze/2;
+			camera[Y] = wMaze/2;
+			camera[Z] = 100;
+			focus[X] = hMaze/2;
+			focus[Y] =wMaze/2;
+			focus[Z] = 0;
 			break;
-		case GLUT_KEY_F3: // camera lado direito labirinto
-			CAM_ATUAL = DIREITA; //camera do lado direito do labirinto
-			break;
-		case GLUT_KEY_F4: //camera na frente do labirinto
-			CAM_ATUAL = CIMA; //camera "em cima" do labirinto
-			break;
-		case GLUT_KEY_F5: //camera lado esquerdo labirinto
-			CAM_ATUAL = ESQUERDA; //camera do lado esquerdo do labirinto
-			break;
-
-		case GLUT_KEY_DOWN: //anda pra baixo no mapa
-			if(posRobo[X]+2 <= hMaze){
-				if(labirinto[posRobo[X]+2][posRobo[Y]] == 1){
-					if(labirinto[posRobo[X]+2][posRobo[Y]-1] == 1 && labirinto[posRobo[X]+2][posRobo[Y]+1] == 1 ){
-						//se caiu aqui pode ir pra baixo
-						printf("Pode BAIXO\n");
-						if(lastMotion == BAIXO || lastMotion == -1){
-							lastMotion = BAIXO;
-							posRobo[X]++;
-							rotateRobo = -90;
-						}else if(lastMotion == CIMA){
-							lastMotion = CIMA;
-							rotateRobo = 90;
-							printf("BAIXO 180\n");
-						}else if(lastMotion == ESQUERDA){
-							lastMotion = BAIXO;
-							rotateRobo = 180;
-							printf("BAIXO 90\n");
-						}else if(lastMotion == DIREITA){
-							lastMotion = BAIXO;
-							rotateRobo = 0;
-							printf("BAIXO -90\n");
-						}
-						lastMotion = BAIXO;
-					}
-				}
-			}
-			break;
-		case GLUT_KEY_UP: //anda pra cima no mapa
-			if(posRobo[X]-2 >=0){
-				if(labirinto[posRobo[X]-2][posRobo[Y]] == 1){
-					if(labirinto[posRobo[X]-2][posRobo[Y]-1] == 1 && labirinto[posRobo[X]-2][posRobo[Y]+1] == 1){
-						printf("Pode CIMA\n");
-						if(lastMotion == CIMA || lastMotion == -1){
-							posRobo[X]--;
-							lastMotion = CIMA;
-							rotateRobo = 90;
-						}else if(lastMotion == BAIXO){
-							lastMotion = CIMA;
-							rotateRobo = -90;
-							printf("CIMA 180\n");
-						}else if(lastMotion == ESQUERDA){
-							lastMotion = CIMA;
-							rotateRobo = 180;
-							printf("CIMA -90\n");
-						}else if(lastMotion == DIREITA){
-							lastMotion = CIMA;
-							rotateRobo = -90;
-							printf("CIMA 90\n");
-						}
-						lastMotion = CIMA;
-					}
-				}
-			}
-			break;
-		case GLUT_KEY_LEFT: //anda pra esquerda no mapa
-			if(posRobo[Y]-2 >= 0){
-				if(labirinto[posRobo[X]][posRobo[Y]-2] == 1){
-					if(labirinto[posRobo[X]+1][posRobo[Y]-2] == 1 && labirinto[posRobo[X]-1][posRobo[Y]-2] == 1){
-						printf("Pode ESQUERDA\n");
-						if(lastMotion == ESQUERDA || lastMotion == -1){
-							posRobo[Y]--;
-							lastMotion = ESQUERDA;
-							rotateRobo = 180;
-						}else if(lastMotion == CIMA){
-							lastMotion = ESQUERDA;
-							rotateRobo = 90;
-							printf("ESQUERDA 90\n");
-						}else if(lastMotion == BAIXO){
-							lastMotion = ESQUERDA;
-							rotateRobo = -90;
-//							printf("ESQUERDA -90\n");
-						}else if(lastMotion == DIREITA){
-							lastMotion = ESQUERDA;
-							rotateRobo = 0;
-//							printf("ESQUERDA 180\n");
-						}
-						lastMotion = ESQUERDA;
-					}
-				}
-			}
-			break;
-		case GLUT_KEY_RIGHT: //anda pra direita no mapa
-			//se tiver mais duas posicoes pra direita no mapa
-			if(posRobo[Y]+2 < wMaze){
-				if(labirinto[posRobo[X]][posRobo[Y]+2] == 1){
-					if(labirinto[posRobo[X]+1][posRobo[Y]+2] == 1 && labirinto[posRobo[X]-1][posRobo[Y]+2] == 1){
-						printf("Pode DIREITA\n");
-						if(lastMotion == DIREITA || lastMotion == -1){
-							//continua pro mesmo lado, nao precisa rotacao
-							//atualiza posicao robo
-							posRobo[Y]++;
-							lastMotion = DIREITA;
-							rotateRobo = 0;
-						}else if(lastMotion == ESQUERDA){
-//							posRobo[Y]++;
-							lastMotion = DIREITA;
-							rotateRobo = 180;
-							printf("DIREITA 180\n");
-						}else if(lastMotion == CIMA){
-							lastMotion = DIREITA;
-							rotateRobo = 90;
-							printf("DIREITA -90\n");
-						}else if(lastMotion == BAIXO){
-							lastMotion = DIREITA;
-							rotateRobo = -90;
-							printf("DIREITA 90\n");
-						}
-						lastMotion = DIREITA;
-					}
-				}
-			}
-			break;
-		case GLUT_KEY_PAGE_UP: //rotaciona Z
-			rotZ++;
-			break;
-		case GLUT_KEY_PAGE_DOWN: //rotaciona -Z
-			rotZ--;
+		case GLUT_KEY_F3: //camera padrao
+			CAM_ATUAL = CAM_TRAS;
+		case GLUT_KEY_F4: //camera atras do robo estatica olhando o labirinto
+			CAM_ATUAL = CAM_ROBO; //camera atras do robo
 			break;
 	}
 	glutPostRedisplay();
@@ -446,27 +275,23 @@ void specialKeys(int key, int x, int y){
 
 void drawMaze( double w, double h){
 	glPushMatrix();
-	glTranslatef(0,0,ALTURA_PAREDE/2);
-	//para cada linha da matriz do labirinto
+	glTranslatef(0,0,1.5);
 	for(int i = 0; i < h; i++){
-		//para cada coluna da matriz do labirinto
 		for(int j = 0; j < w; j++){
-			//se posicao do labirinto for uma parede
-			if(labirinto[i][j] == 0){
-				//desenhar parede
+			if(labirinto[i][j] == 0){ //se posicao do labirinto for uma parede
 				glPushMatrix();
 					glColor3f(1,0,0);
 					glScalef(1,1,ALTURA_PAREDE);
 					glTranslatef(i,j,0);
 					glutSolidCube(1);
 				glPopMatrix();
-			}else if(labirinto[i][j] == CONE){
+			}else if(labirinto[i][j] == CONE){ //se for objeto CONE
 				glPushMatrix();
 					glColor3f(1,0.6,0.5);
 					glTranslatef(i,j,-1.5);
 					glutSolidCone(1,ALTURA_PAREDE,50,50);
 				glPopMatrix();
-			}else if(labirinto[i][j] == TORUS){
+			}else if(labirinto[i][j] == TORUS){ //se for objeto TORUS
 				glPushMatrix();
 					glColor3f(1,1,0);
 					glTranslatef(i,j,0);
@@ -474,7 +299,7 @@ void drawMaze( double w, double h){
 					glRotatef(rotateTORUS++,1,0,0);
 					glutSolidTorus(0.3, 0.7,50,50);
 				glPopMatrix();
-			}else if(labirinto[i][j] == TEAPOT){
+			}else if(labirinto[i][j] == TEAPOT){ //se for objeto CHALEIRA
 				glPushMatrix();
 					glColor3f(0.2,0.2,0.2);
 					glTranslatef(i,j,0);
@@ -489,18 +314,11 @@ void drawMaze( double w, double h){
 };
 
 void drawRobot(){
-
-
-
-
 	glPushMatrix();
 		glTranslatef(posRobo[X],posRobo[Y],posRobo[Z]);
 		//rotacoes conforme teclado
-		printf("ROOOOT %f\n", rotateRobo);
-
 		glRotatef(angleRotateRobot,0,0,1);
-
-		//Pernas
+		//PERNAS
 		glColor3f(0.4f,0.4f,0.4f);
 		glPushMatrix();
 			glTranslatef(0.4,0,2);
@@ -514,15 +332,13 @@ void drawRobot(){
 			glRotatef(-180,1,0,0);
 			gluCylinder(leg, 0.15, 0.15, 2, 50,50);
 		glPopMatrix();
-
-//		TRONCO
+		//TRONCO
 		glColor3f(0.7f,0.7f,0.7f);
 		glPushMatrix();
 			glTranslatef(0, 0, 0.25*13);
 			glScalef(2,1,3);
 			glutSolidCube(1);
 		glPopMatrix();
-
 		//BRAÇOS
 		glColor3f(0.2f,0.2f,0.2f);
 		//BRAÇO DIREITO
@@ -545,24 +361,20 @@ void drawRobot(){
 			glScalef(1,2,10);
 			gluCylinder(arm,0.15,0.15, 0.25, 50,50);
 		glPopMatrix();
-
 		//PESCOÇO
 		glColor3f(0.25f,0.25f,0.25f);
 		glPushMatrix();
 			glTranslatef(0, 0, 0.25*19);
 			gluCylinder(neck,0.55,0.33,1.25,40,40);
 		glPopMatrix();
-
 		//CABEÇA
 		glPushMatrix();
 			glRotatef(rotateHead,0,0,1);
 			glColor3f(0.65f,0.55f,0.44f);
 			glPushMatrix();
 				glTranslatef(0, 0, 0.25*24);
-		//		glScalef(1,1,1.75);
 				glutSolidSphere(0.75,50,50);
 			glPopMatrix();
-
 			//ORELHA
 			glColor3f(0.25f,0.25f,0.25f);
 			//ORELHA DIREITA
@@ -579,7 +391,6 @@ void drawRobot(){
 				glScalef(0.75,0.75,0.75);
 				glutSolidTorus(0.25,0.5,50,50);
 			glPopMatrix();
-
 			//OLHOS
 			glColor3f(1,1,1);
 			//OLHO DIREITO
@@ -594,42 +405,26 @@ void drawRobot(){
 			glPopMatrix();
 		glPopMatrix();
 	glPopMatrix();
-
-
 };
 
-
-
-
 //##########################//
-//$$$$$$$$$$$$$$$$$$$$$$$$$$//
 //			MAIN			//
-//$$$$$$$$$$$$$$$$$$$$$$$$$$//
 //##########################//
 int main(int argc, char **argv) {
-
-	//inicializa GLUT com parametros necessarios
-
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-	//cria JANELA DE VISUALIZACAO
 	glutInitWindowPosition(windowPosX,windowPosY);
 	glutInitWindowSize(windowWidth,windowHeight);
-	glutCreateWindow("Labirinto 3D");
-
+	glutCreateWindow("Computação Gráfica - Trabalho 1 - Labirinto 3D");
 	glClearColor(0,0,0,1);
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
-
 	//FUNCOES DE CALLBACK
 	glutReshapeFunc(reshapeWindow);
-	glutTimerFunc(150, animate, 1);
+	glutTimerFunc(45, animate, 1);
 	glutDisplayFunc(draw);
 	glutSpecialFunc(specialKeys);
 	glutFullScreen();
 	glutKeyboardFunc(teclado);
-
 	glutMainLoop();
 }
-
